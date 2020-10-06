74a09b42904759fc79d996b6f354213bbc7a43f1
jrcliff <justoon7.jc@gmail.com>
2020-10-06 12:59:38 -0400

    good practice

--- newsletter.rb
+++ newsletter.rb
@@ -3,7 +3,7 @@ require 'pry'
 #########################
 # Data for the newsletter
 #########################
-require "pry"
+
 CAMPUS = {
   "name": "DC",
   "address": "1440 G St NW, Washington, DC 20005",
@@ -28,33 +28,49 @@ ARTICLES = [
 #########################
 
 def calculate_recipients
+ # SUBSCRIBERS.select do |element|
+    SUBSCRIBERS - UNSUBSCRIBED
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # write a method that will return an array of only the subscribers who haven't unsubscribed
 end
 
-def first_n_articles(number_of_articles
+def first_n_articles(number_of_articles)
   ARTICLES.first(number_of_articles)
+
 end
+#binding.pry
+
 
 def print_recipients
+  calculate_recipients.join(", ")
   # Write a method that uses the output of calculate_recipients
   # and returns a list of emails separated by commas
   # Ex) "abc@email.com, def@email.com, ghi@email.com"
 end
-
+#binding.pry
 def print_one_article(article)
+  puts "#{article[:title]}" 
+  puts "by: #{article[:author]}" 
+  puts "#{article[:text]}"
+  #binding.pry
   # Write a method that will take an article hash
   # and print the title, author and text as a formatted string
   # See the README/sample output for examples
 end
+#binding.pry
 
 def print_many_articles(articles)
+  articles.each do |index|
+    print_one_article(index)
+    puts " "
+  end
+  
   # Write a method that will take in an array of article hashes
   # and format each one using the print_one_article method
 end
 
 def format_campus_location(campus)
-  "Flatiron #{campus["name"]}"
+  "Flatiron #{campus[:name]}"
 end
 
 def format_subject
@@ -80,13 +96,14 @@ def print_newsletter(number)
   print_many_articles(articles)
   puts format_footer(CAMPUS)
 
-  end
+  
 end
+#binding.pry
 
 def run
   # We want our program to print three articles by default,
   # but we can change that number here
-  print_newsletter("3")
+  print_newsletter(3)
 end
 
 # When we run "ruby newsletter.rb" in the command line,
